6d6dad83773f088d01619c618a0198206db8c59a
HUI NIU <hui-niu@outlook.com>
2022-09-04 16:11:48 +1200

    Updated the back-end project

---
+++ Back-end/Back-end/Data/Back_endContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Back_end.Models;
+
+namespace Back_end.Data
+{
+    public class Back_endContext : DbContext
+    {
+        public Back_endContext (DbContextOptions<Back_endContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Back_end.Models.Product> Product { get; set; }
+    }
+}
